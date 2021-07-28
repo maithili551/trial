30db2b8a2977faa5b9ee80375a2a805636bd8dab
maithili551 <62747114+maithili551@users.noreply.github.com>
2021-07-28 18:30:02 +0530

    added class

--- Hello.java
+++ Hello.java
@@ -1,2 +1,10 @@
-System.out.println("Hello!");
-System.out.println("How are you");
+class Hello
+{
+  public static void main (String[]
+args)
+  {
+    System.out.println("Hello!");
+    System.out.println("How are you");
+  }
+}
+
